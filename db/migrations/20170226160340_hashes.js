d756e6f6193b0e6207874a83219dd3c94fd14849
Tommy Toomean <arttumin@gmail.com>
2017-02-26 16:23:42 +0300

    +add timestamps default values

--- db/migrations/20170226160340_hashes.js
+++ db/migrations/20170226160340_hashes.js
@@ -3,14 +3,14 @@ exports.up = function(knex, Promise)
 {
 	return Promise.resolve()
 	.then(() =>
-	{
+	{	
 		return knex.schema.createTable('hashes', table =>
 		{
 			table.increments('id').primary()
 			table.string('origin', 2000)
 			table.string('hash')
-			table.timestamp('created_at')
-			table.timestamp('expired_at')
+			table.timestamp('created_at').defaultTo(knex.fn.now())
+			table.timestamp('expired_at').notNullable()
 		})
 	})  
 }

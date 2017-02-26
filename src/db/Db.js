720276c9d0532262c4712dd066ec0b58aa0b22df
Tommy Toomean <arttumin@gmail.com>
2017-02-26 16:53:58 +0300

    +hashes model

--- src/db/Db.js
+++ src/db/Db.js
@@ -2,11 +2,17 @@
 
 const Pg = require('./Pg')
 
+const Hash = require('./models/Hash')
+
 module.exports = function Db (cfg)
 {
 	let db = {}
 
 	db.pg = Pg(cfg)
+
+	db.knex = db.pg.kx
+
+	db.hash = Hash(db)
 
 	return db
 }
---
+++ src/db/models/Hash.js
@@ -0,0 +1,25 @@
+/* @flow */
+
+module.exports = function Hash (db)
+{
+	let hash = {}
+
+	let knex = db.knex
+
+	hash.create = () =>
+	{
+
+	}
+
+	hash.get = (hash) =>
+	{
+
+	}
+
+	hash.delete = () =>
+	{
+
+	}
+
+	return hash
+}
